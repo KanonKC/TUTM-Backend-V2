02875ea95af9544fdc11399ccde1c3616626cc6e
KanonKC <kanonkce@gmail.com>
2024-08-26 19:32:46 +0700

    Add build file

--- src/tests/services/Youtube.test.ts
+++ src/tests/services/Youtube.test.ts
@@ -1,9 +1,9 @@
-import { searchPlaylist, searchVideo } from "../../services/Youtube.service";
+// import { searchPlaylist, searchVideo } from "../../services/Youtube.service";
 
-searchVideo("avenger").then((data) => {
-    console.log(data)
-})
+// searchVideo("avenger").then((data) => {
+//     console.log(data)
+// })
 
-searchPlaylist("RDrjyvSyl5eEw").then((data) => {
-    console.log(data)
-})
+// searchPlaylist("RDrjyvSyl5eEw").then((data) => {
+//     console.log(data)
+// })
