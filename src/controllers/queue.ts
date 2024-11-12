58a45a7114df2696db1e5eb68e90f846506e5b13
KanonKC <kanonkce@gmail.com>
2024-11-12 15:37:44 +0700

    Play next music when current queue is deleted

--- src/controllers/queue.ts
+++ src/controllers/queue.ts
@@ -1,6 +1,7 @@
 import { Queue, YoutubeVideo } from "@prisma/client";
 import { prisma } from "../prisma";
 import { getYoutubeVideoData } from "../services/Youtube.service";
+import { playNext } from "./playlist";
 
 export async function getAllQueuesInPlaylist(playlistId: string) {
 	const queues = await prisma.queue.findMany({
@@ -48,9 +49,11 @@ export async function addVideoToQueue(playlistId: string, videoId: string) {
 	}
 
 	const queues = await getAllQueuesInPlaylist(playlistId);
-    console.log(queues);
+	console.log(queues);
 	const nextOrder =
-		queues.length === 0 ? 0 : Math.max(...queues.map((q) => q.order ?? -1)) + 1;
+		queues.length === 0
+			? 0
+			: Math.max(...queues.map((q) => q.order ?? -1)) + 1;
 	const video = videoModel;
 
 	const queue = await prisma.queue.create({
@@ -90,16 +93,29 @@ export async function getQueueById(queueId: string) {
 }
 
 export async function deleteQueueById(queueId: string) {
-	const queue = await prisma.queue.delete({
-        where: { id: queueId },
-    });
-    const queueList = await getAllQueuesInPlaylist(queue.playlistId);
-    for(let i = 0; i < queueList.length; i++){
-        await prisma.queue.update({
-            where: { id: queueList[i].id },
-            data: { order: i },
-        });
-    }
+	const queue = await prisma.queue.findUniqueOrThrow({
+		where: { id: queueId },
+		include: { playlist: true },
+	});
+
+	if (queue.playlist.currentQueueId === queueId) {
+		try {
+			await playNext(queue.playlistId);
+		} catch (error) {}
+	}
+	await prisma.queue.delete({
+		where: { id: queueId },
+	});
+
+	const queueList = await getAllQueuesInPlaylist(queue.playlistId);
+
+	for (let i = 0; i < queueList.length; i++) {
+		await prisma.queue.update({
+			where: { id: queueList[i].id },
+			data: { order: i },
+		});
+	}
+
 	return true;
 }
 
@@ -111,22 +127,25 @@ export async function increaseQueuePlayedCount(queueId: string) {
 }
 
 export interface ReOrderQueuePayload {
-    queues: {
-        queueId: string;
-        order: number;
-    }[]
+	queues: {
+		queueId: string;
+		order: number;
+	}[];
 }
 
-export async function reOrderQueue(playlistId: string, payload: ReOrderQueuePayload) {
-    await prisma.queue.updateMany({
-        where: { playlistId },
-        data: { order: null },
-    });
-    for(let i = 0; i < payload.queues.length; i++){
-        await prisma.queue.update({
-            where: { id: payload.queues[i].queueId },
-            data: { order: payload.queues[i].order },
-        });
-    }
-    return true;
+export async function reOrderQueue(
+	playlistId: string,
+	payload: ReOrderQueuePayload
+) {
+	await prisma.queue.updateMany({
+		where: { playlistId },
+		data: { order: null },
+	});
+	for (let i = 0; i < payload.queues.length; i++) {
+		await prisma.queue.update({
+			where: { id: payload.queues[i].queueId },
+			data: { order: payload.queues[i].order },
+		});
+	}
+	return true;
 }
